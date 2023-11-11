1dbb19ec984afdc06ea187e1d75740e76d9e5e33
Tasty <yusufkarakoc06@hotmail.com>
2023-11-11 21:50:23 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 import axios from "axios";
 
-const isDev = import.meta.env?.DEV;
 
 const instance = axios.create({
   baseURL: `https://confessions-server.codederin.repl.co/api`,
