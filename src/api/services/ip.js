3e492924c1760d2fecd40fd5a0365ec35c5a7aea
Tasty <yusufkarakoc06@hotmail.com>
2023-11-11 22:00:26 +0300

    Update ip.js

--- src/api/services/ip.js
+++ src/api/services/ip.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-undef */
 import axios from "axios";
 
-const isDev = import.meta.env?.DEV;
 
 const getIP = async () => {
   const ip = await axios
     .get(
-      `https://geolocation-db.com/json/fd18cb60-5f5a-11ee-87d3-bd3f0d7c4f89`
+      `https://geolocation-db.com/json/`
     )
     .then((res) => res.data.IPv4)
     .catch((err) => console.error(`${err.code}: - ${err.message}`));
