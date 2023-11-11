19b06476c8aa309298217ee6334dce70c96b4e58
Tasty <yusufkarakoc06@hotmail.com>
2023-11-11 22:14:57 +0300

    Update ip.js

--- src/api/services/ip.js
+++ src/api/services/ip.js
@@ -2,19 +2,11 @@
 import axios from "axios";
 
 const getIP = async () => {
-  try {
-    const response = await axios.get("https://geolocation-db.com/json/fd18cb60-5f5a-11ee-87d3-bd3f0d7c4f89");
-    
-    // İp adresini doğrudan döndürmek yerine response içinden alabilirsiniz.
-    const ip = response.data.IPv4;
-
-    return ip;
-  } catch (error) {
-    // Hata durumunda daha geniş bir hata mesajı yazdırabilirsiniz.
-    console.error(`Error fetching IP: ${error.message}`);
-    // Hata durumunda null veya başka bir değer döndürebilirsiniz, bağlamınıza bağlı olarak.
-    return null;
-  }
+  const ip = await axios
+    .get(`https://geolocation-db.com/json/fd18cb60-5f5a-11ee-87d3-bd3f0d7c4f89`)
+    .then((res) => res.data.IPv4)
+    .catch((err) => console.error(`${err.code}: - ${err.message}`));
+  return ip;
 };
 
 export { getIP };
