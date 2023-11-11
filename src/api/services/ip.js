f3ace18fa96e951742d311b275323f80198d8622
Tasty <yusufkarakoc06@hotmail.com>
2023-11-11 23:03:41 +0300

    Update ip.js

--- src/api/services/ip.js
+++ src/api/services/ip.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const getIP = async () => {
   const ip = await axios
-    .get(`https://geolocation-db.com/json/fd18cb60-5f5a-11ee-87d3-bd3f0d7c4f89`)
-    .then((res) => res.data.IPv4)
+    .get(`http://ip-api.com/json/`)
+    .then((res) => res.data.query)
     .catch((err) => console.error(`${err.code}: - ${err.message}`));
   return ip;
 };
