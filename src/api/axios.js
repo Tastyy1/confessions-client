a212c924b4d548734997cfa18a115609a9260091
Tasty <yusufkarakoc06@hotmail.com>
2023-11-11 20:15:00 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-undef */
 import axios from "axios";
 
+const isDev = import.meta.env?.DEV;
 
 const instance = axios.create({
-  baseURL: `https://confessions-client-nine.vercel.app/api`,
+  baseURL: `${
+    isDev ? import.meta.env.VITE_API_URL : process.env.REACT_APP_API_URL
+  }/api`,
 });
 
 export default instance;
