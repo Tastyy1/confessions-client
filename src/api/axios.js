b0068f90b680bed72a83829af11a95e25d85bfbb
Tasty <yusufkarakoc06@hotmail.com>
2023-11-11 20:53:50 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,9 @@ import axios from "axios";
 const isDev = import.meta.env?.DEV;
 
 const instance = axios.create({
-  baseURL: `https://confessions-server.vercel.app/api`, 
+  baseURL: `${
+    isDev ? import.meta.env.VITE_API_URL : process.env.REACT_APP_API_URL
+  }/api`,
 }); 
 
 export default instance;
