073b30ca82b78bb11fe4da21e3f06f73652d88b5
Tasty <yusufkarakoc06@hotmail.com>
2023-11-11 23:06:03 +0300

    Update ip.js

--- src/api/services/ip.js
+++ src/api/services/ip.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const getIP = async () => {
   const ip = await axios
-    .get(`http://ip-api.com/json/`)
+    .get(`https://ip-api.com/json/`)
     .then((res) => res.data.query)
     .catch((err) => console.error(`${err.code}: - ${err.message}`));
   return ip;
