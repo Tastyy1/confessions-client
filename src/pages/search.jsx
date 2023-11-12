3a9eba8b8ee6e6e73a0681f4e8af80d58a000561
Tasty <yusufkarakoc06@hotmail.com>
2023-11-12 16:57:48 +0300

    Update search.jsx

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -40,7 +40,7 @@ export default function Search() {
             type="search"
             id="default-search"
             className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Search"
+            placeholder="Ara"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             required
