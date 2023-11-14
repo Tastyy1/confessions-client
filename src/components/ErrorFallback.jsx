d967695d534c05018a9faf8cedc0a0492182362c
Tasty <yusufkarakoc06@hotmail.com>
2023-11-14 20:07:43 +0300

    Update ErrorFallback.jsx

--- src/components/ErrorFallback.jsx
+++ src/components/ErrorFallback.jsx
@@ -1,21 +1,11 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function ErrorFallback({ error, resetErrorBoundary }) {
-  const history = useHistory();
 
-  const handleRetry = () => {
-    // Hata mesajı "HMM KNK SANIRIM BANLANDIN YA. AH BE" ise
     if (error?.response?.data?.message === "HMM KNK SANIRIM BANLANDIN YA. AH BE") {
-      // Kullanıcıyı istediğiniz linke yönlendir
-      window.location.href = "https://youtu.be/KkIyJWVJAAY"; // Örnek: YouTube'a yönlendirme
-    } else {
-      // Diğer durumlarda normal tekrar deneme işlemi
-      resetErrorBoundary();
-    }
-  };
+      window.location.href = "https://youtu.be/KkIyJWVJAAY";
+    } 
 
   return (
     <>
@@ -23,10 +13,8 @@ export default function ErrorFallback({ error, resetErrorBoundary }) {
 
       <div>
         <h1>Opps! Bir şeyler ters gitti!</h1>
-        <pre>{error?.response?.data?.message}</pre>
-        <button className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={handleRetry}>
-          TEKRAR DENE
-        </button>
+        <h1>{error?.response?.data?.message}</h1>
+        <button className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={resetErrorBoundary}>TEKRAR DENE</button>
       </div>
 
       <Footer />
